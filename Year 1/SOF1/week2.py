daabc2a68ba15f9d91b0c1ee736f4ad83dd4aa72
Kieron Jones <94188083+KieronJones@users.noreply.github.com>
2022-06-03 15:21:11 +0100

    SOF1 Week2 complete

--- Year 1/SOF1/week2.py
+++ Year 1/SOF1/week2.py
@@ -1,11 +1,39 @@
-# Exercise 1
-
-num_stones = int(input("Please enter a number."))
-num_pounds = int(input("Please enter a number of pounds."))
-
-num_kilos = (num_stones * 6.35029318) + (num_pounds * 0.45359237)
-
-print(str(num_stones) + " stone and " + str(num_pounds) + " pounds in kilos is " + str(num_kilos) + " kg.")
+# Exercise 1 & 2
+def stones_and_pounds_to_kilos(stones, pounds):
+    num_kilos = (stones * 6.35029318) + (pounds * 0.45359237)
+    message = (str(stones) + " stone and " + str(pounds) + " pounds in kilos is " + str(num_kilos) + " kg.")
+    return message
 
 
-num_kilos = int(input("Please enter a number of kilos."))
+def kilos_to_stones_and_pounds(kilos):
+    stones = kilos // 6.35029318
+    pounds = (((kilos / 6.35029318) % 1) * 14)
+    message = (str(kilos) + " kilos in stones and pounds is " + str(stones) + " and " + str(pounds) + " pounds.")
+    return message
+
+
+#Exercise 3
+def bananas_to_cost(kilos):
+    order = (kilos*3) + 4.99
+    if order > 50:
+        order -= 1.50
+    return order
+
+
+#Exercise 4
+def training_zone(age, rate):
+    m = 208 - (0.7 * age)
+    if (rate >= (0.9*m)):
+        print("Interval Training")
+    elif ((0.7 * m) <= rate < (0.9 * m)):
+        print("Threshold Training")
+    elif ((0.5 * m) <= rate < (0.7 * m)):
+        print("Aerobic Training")
+    else:
+        print("Couch Potato")
+
+
+#Exercise 5
+def length_to_area(a, b, c):
+    s = (a + b + c)/2
+    return (s*(s-a)*(s-b)*(s-c))**0.5
