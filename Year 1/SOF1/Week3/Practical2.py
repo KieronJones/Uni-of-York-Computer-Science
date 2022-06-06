b350007db333f87c685a82b33f248621cad144b2
Kieron Jones <94188083+KieronJones@users.noreply.github.com>
2022-06-06 21:47:24 +0100

    finished practical 2

--- Year 1/SOF1/Week3/Practical2.py
+++ Year 1/SOF1/Week3/Practical2.py
@@ -24,4 +24,33 @@ def split_camel_case(text):
     return re.findall('[A-Z][^A-Z]*', text)
 
 
-#Exercise 3
+#Exercise 3 - Part 1
+def ceaser_encrypt(text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shift = 3
+    cypher_text = ""
+    for letter in text:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            substitution_index = (index + shift) % len(alphabet)
+            cypher_text = cypher_text + alphabet[substitution_index]
+        else:
+            cypher_text = cypher_text + letter
+
+    return cypher_text
+
+
+#Exercise 3 - Part 2
+def ceaser_decrypt(text):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    shift = 3
+    cypher_text = ""
+    for letter in text:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            substitution_index = (index - shift) % len(alphabet)
+            cypher_text = cypher_text + alphabet[substitution_index]
+        else:
+            cypher_text = cypher_text + letter
+
+    return cypher_text
